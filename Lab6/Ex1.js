2df6f4c56f17822be3d2c3eaec53d2b146057f70
alvinalmira <aepa102@gmail.com>
2020-09-23 07:53:27 +1000

    Create Ex1.js

---
+++ Lab6/Ex1.js
@@ -0,0 +1,9 @@
+var age =  20;
+var fav_num = 21;
+var day_of_birth = 21;
+var month_of_birth = 3;
+
+console.log(age + fav_num / day_of_birth * month_of_birth);
+console.log("\n");
+console.log( (age + fav_num) / day_of_birth * month_of_birth);
+
