c344a97c7c929d62b363d02ab7bbf7e2eb736b85
alvinalmira <aepa102@gmail.com>
2020-09-30 07:50:03 +1000

    ex2

--- Lab8/Ex1.js
+++ Lab8/Ex1.js
@@ -1,6 +1,10 @@
 counter = 1 // starting an age counter
 age = 20
 while ( counter < 20 ) {
+    if (counter >= age/2){
+        console.log("I'm old!");
+        break;
+    }
     console.log(`age ${counter}`);
     counter++;
 }
