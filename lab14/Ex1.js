5757bba0ab39651fabb9e4b997d9931b176ee35f
alvinalmira <aepa102@gmail.com>
2020-11-20 02:11:47 +1000

    lab14

--- lab14/Ex1.js
+++ lab14/Ex1.js
@@ -8,4 +8,13 @@ user_reg_data = JSON.parse(data);
 
 // console.log(user_reg_data, typeof user_reg_data, typeof data);
 
-console.log(user_reg_data['dport']['password'], typeof user_reg_data['dport']['password']);
+// console.log(user_reg_data['dport']['password'], typeof user_reg_data['dport']['password']);
+
+// checks if the user exists; if they exist, get the password
+if (typeof user_reg_data['itm352'] != undefined) {
+    console.log(user_reg_data['itm352']['password']=='grader');
+}
+
+// to check if a user exists statement
+// if (typeof user_reg_data['itm352'] == undefined) {};
+    
